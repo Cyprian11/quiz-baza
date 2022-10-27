0082c745bff076ecc723b5764133a708b75e7a6c
Cyprian11 <barabaszcyprianti@gmail.com>
2022-10-27 09:50:25 +0200

    baza pelna

---
+++ quiz.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    
+    
+    
+    
+    </head>
+    <body>
+    <?php
+        $con = new mysqli("127.0.0.1","Administrator","zsp","quiz");
+
+        $ans="SELECT "
+
+
+        ?>
+    
+    </body>
+</html>
